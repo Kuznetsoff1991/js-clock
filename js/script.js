71eab726840832aff0c98295d00b359197fe7e87
Kuznetsoff1991 <dmitriykuznetsov91@gmail.com>
2019-02-21 13:03:08 +0300

    Часы

--- js/script.js
+++ js/script.js
@@ -1,95 +1,23 @@
-window.addEventListener('DOMContentLoaded', function() {
-    'use ctrict';
+let day = new Date(),
+  hour,
+  minutes,
+  seconds,
+  div = document.querySelector('div');
 
-    // Табы
-    let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent (a) {
-        for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-        }
-    }
-
-    hideTabContent(1);
-
-    function showTabContent(b) {
-        if (tabContent[b].classList.contains('hide')) {
-            tabContent[b].classList.remove('hide');
-            tabContent[b].classList.add('show');
-        }
-    }
-
-    info.addEventListener('click', function (e) {
-        let target = e.target;
-        if (target && target.classList.contains('info-header-tab')) {
-            for(let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
-                    break;
-                }
-            }
-        }
-    });
-
-    // Таймер обратного отсчета
-    let deadLine = '2018-10-27';
-
-    function getTimeRemaining(endTime) {
-        let t = Date.parse(endTime) - Date.parse(new Date()),
-            seconds = Math.floor((t / 1000) % 60),
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            hours = Math.floor(t / 1000 / 60 / 60);
-
-        if (Date.parse(endTime) < Date.parse(new Date())) {
-            seconds = '00';
-            minutes = '00';
-            hours = '00';
-        }
-
-        if (Math.abs(seconds) < 10) {
-            seconds = '0' + Math.abs(seconds);
-        } else {
-            seconds = '' + Math.abs(seconds);
-        }
-        if (Math.abs(minutes) < 10) {
-            minutes = '0' + Math.abs(minutes);
-        } else {
-            minutes = '' + Math.abs(minutes);
-        }
-        if (Math.abs(hours) < 10) {
-            hours = '0' + Math.abs(hours);
-        } else {
-            hours = '' + Math.abs(hours);
-        }
-
-        return ({
-            'total': t,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        });
-    }
-
-    function setClock(id, endTime) {
-        let timer = document.getElementById(id),
-            hours = timer.querySelector('.hours'),
-            minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds'),
-            timeInterval = setInterval(updateClock, 1000);
-
-        function updateClock() {
-            let t = getTimeRemaining(endTime);
-
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
-        }
-
-    }
-
-    setClock('timer', deadLine);
-});
+setInterval(function () {
+  day = new Date();
+  hour = day.getHours();
+  minutes = day.getMinutes();
+  seconds = day.getSeconds();
+  if (hour < 10) {
+    hour = '0' + hour;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  div.textContent = hour + ':' + minutes + ':' + seconds;
+}, 1000);
